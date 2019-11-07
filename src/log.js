85d0a42a33fb97e37143e91eef7d896d65b6826f
Tony Spataro <xeger@xeger.net>
2019-11-06 18:47:00 -0800

    Fix Sentry error reporting

--- src/log.js
+++ src/log.js
@@ -27,11 +27,17 @@ export function fatal(exception) {
   let id;
 
   // Report to Sentry using the unified SDK or (legacy) Raven SDK.
-  // Capture the error ID if it was made available to us.
+  // Capture the error ID if it was made available to us. Paranoia
+  // checks are present so we work with a wide variety of SDK
+  // versions.
   const sentry = window.Sentry || window.Raven;
   if (sentry && typeof sentry.captureException === 'function') {
-    sentry.captureException('moo');
-    if (typeof sentry.lastEventId === 'function') id = sentry.lastEventId();
+    const hasTags = typeof sentry.setTagsContext === 'function';
+    const hasId = typeof sentry.lastEventId === 'function';
+    if (hasTags) sentry.setTagsContext({ sylph: true });
+    sentry.captureException(exception);
+    if (hasTags) sentry.setTagsContext({ sylph: false });
+    if (hasId) id = sentry.lastEventId();
   }
 
   if (id) info(`(id=${id})`, exception);
