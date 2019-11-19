849cc75a106f8e42d07a591f346c129ded787296
Tony Spataro <xeger@xeger.net>
2019-11-18 16:35:11 -0800

    Fix sirv workaround for electron.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,5 +5,5 @@ Cypress.on('uncaught:exception', err => {
   // sirv-cli responds with index.html instead of 404 when something's not found.
   // not useful for testing (or for anything, really) but until we bother to
   // write our own sirv script, this is a handy workaround.
-  if (err.message && err.message.includes("Unexpected token '<'")) return false;
+  if (/Unexpected token '?<'?/.match(err.message)) return false;
 });
